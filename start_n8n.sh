da67a841fe829a27727621a25b4b478561adb37c
Rocket-Nice <fjobfrom@gmail.com>
2025-11-26 19:32:34 +0300

    123

---
+++ start_n8n.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run -it --rm --name n8n \
+  -p 5678:5678 \
+  -v ~/n8n_data:/home/node/.n8n \
+  -e N8N_BASIC_AUTH_ACTIVE=true \
+  -e N8N_BASIC_AUTH_USER=admin \
+  -e N8N_BASIC_AUTH_PASSWORD=password123 \
+  n8nio/n8n
